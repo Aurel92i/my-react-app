82d4b9b16ef57b6df8fb5382a2c7ab2b10a69d84
Aurelien <aurelien.herbin2@gmail.com>
2024-09-25 21:14:35 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,38 @@
+const CACHE_NAME = 'static-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png',
+  '/manifest.json'
+];
+
 // Installer le service worker et mettre en cache les fichiers statiques
 self.addEventListener('install', (event) => {
   console.log('Service worker installé.');
   event.waitUntil(
-    caches.open('static-cache-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/favicon.ico',
-        '/logo192.png',
-        '/logo512.png',
-        '/manifest.json'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Activer le service worker
+// Activer le service worker et supprimer les caches obsolètes
 self.addEventListener('activate', (event) => {
   console.log('Service worker activé.');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Ancien cache supprimé:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 // Intercepter les requêtes et renvoyer les fichiers mis en cache si disponibles
