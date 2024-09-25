b3a28202d86a6ac24b776ae3ce8eedc2079d9571
Aurelien <aurelien.herbin2@gmail.com>
2024-09-25 22:00:48 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,23 @@
-const CACHE_NAME = 'static-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/logo192.png',
-  '/logo512.png',
-  '/manifest.json'
-];
-
-// Installer le service worker et mettre en cache les fichiers statiques
 self.addEventListener('install', (event) => {
   console.log('Service worker installé.');
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open('static-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.ico',
+        '/logo192.png',
+        '/logo512.png',
+        '/manifest.json'
+      ]);
     })
   );
 });
 
-// Activer le service worker et supprimer les caches obsolètes
 self.addEventListener('activate', (event) => {
   console.log('Service worker activé.');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Ancien cache supprimé:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
 });
 
-// Intercepter les requêtes et renvoyer les fichiers mis en cache si disponibles
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
